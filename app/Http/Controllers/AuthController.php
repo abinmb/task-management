0d49a9e49c3a8115e784b19fad20b9ba1d6d217a
abinbpract <abin@bpract.com>
2025-03-13 06:46:34 +0530

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'     => 'required|string',
+            'email'    => 'required|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
+    public function login(LoginRequest $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user             = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->plainTextToken;
+            $success['name']  = $user->name;
+
+            return response()->json(['status' => true, 'message' => 'User login successfully', 'data' => $success], 200);
+        } else {
+            return response()->json(['status' => false, 'message' => 'Invalid credentials'], 401);
+        }
+    }
+    public function user(Request $request): JsonResponse
+    {
+        $data = Auth::user();
+        return response()->json(['status' => true, 'message' => 'User login successfully', 'data' => $data], 200);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+      /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert(
+            [
+                [
+                    'email'    => 'admin@gmail.com',
+                    'name'     => 'Admin',
+                    'password' => Hash::make('12345678'),
+                    'role'     => 'admin',
+                ],
+                [
+                    'email'    => 'user@gmail.com',
+                    'name'     => 'User',
+                    'password' => Hash::make('12345678'),
+                    'role'     => 'user',
+                ],
+            ]
+        );
+    }
+}
